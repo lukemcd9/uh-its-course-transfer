266d6b0e616159a48ac297f2efbaaacbd916ecfc
jmaligon <jason.maligon@gmail.com>
2019-08-09 14:48:17 -1000

    Fix Service tests for 100% coverage.

--- src/main/java/edu/hawaii/its/creditxfer/service/EmployeeService.java
+++ src/main/java/edu/hawaii/its/creditxfer/service/EmployeeService.java
@@ -1,7 +1,6 @@
 package edu.hawaii.its.creditxfer.service;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 public interface EmployeeService {
     public boolean exists(String uhuuid);
