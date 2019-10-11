e93eb93a74d87b17fcd1bf88067158671857c37b
jmaligon <jason.maligon@gmail.com>
2019-10-10 16:35:14 -1000

    Filter out 'DO NOT USE' records.

--- src/main/java/edu/hawaii/its/creditxfer/repository/InstitutionRepository.java
+++ src/main/java/edu/hawaii/its/creditxfer/repository/InstitutionRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import edu.hawaii.its.creditxfer.type.Institution;
 
 public interface InstitutionRepository extends JpaRepository<Institution, Integer> {
 
+    @Query("select i from Institution i where i.city <> 'DO NOT USE' or i.city <> 'Do Not Use'")
     List<Institution> findAll();
 
     Optional<Institution> findByCode(String code);
